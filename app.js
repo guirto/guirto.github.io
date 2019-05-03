9f8917188c9020d19d5d83e67553faa2e28517f1
Victor Rodriguez <guirto@gmail.com>
2019-05-03 20:21:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 	var pages = [
 		"Era un d&iacute;a normal en la c&aacute;rcel. Todo lo normal que puede ser un d&iacute;a en un sitio as&iacute;. Est&aacute;bamos animados porque hoy algo cambiaba nuestra rutina. Unos amables se&ntilde;ores ven&iacute;an a visitar nuestras celdas con los chicos de mantenimiento para valorar unas reformas.",
 		"Nos hab&iacute;an hecho salir a la galer&iacute;a y no hab&iacute;a mucho que hacer all&iacute;, salvo rajar con los dem&aacute;s. Uno de los veteranos se acerc&oacute; y empezamos a hablar. Era un tipo raro. Nunca me hab&iacute;a dirigido la palabra.",
-		"Me extra&ntilde;&oacute; que me dijera:<br>&mdash; Llevo tiempo observ&aacute;ndote. &iquest;Te gusta estar aqu&iacute;?<br>&mdash; &iquest;T&uacute; qu&eacute; crees? Esto no tiene pinta de hotel' &mdash; respond&iacute; &mdash;. Adem&aacute;s, no deber&iacute;a estar aqu&iacute;: mi abogado la cag&oacute; &mdash; a&ntilde;ad&iacute; con un gui&ntilde;o."
+		"Me extra&ntilde;&oacute; que me dijera:<br>&mdash; Llevo tiempo observ&aacute;ndote. &iquest;Te gusta estar aqu&iacute;?<br>&mdash; &iquest;T&uacute; qu&eacute; crees? Esto no tiene pinta de hotel' &mdash; respond&iacute; &mdash;. Adem&aacute;s, no deber&iacute;a estar aqu&iacute;: mi abogado la cag&oacute; &mdash; a&ntilde;ad&iacute; con un gui&ntilde;o.",
+		"&mdash; Lo que me imaginaba, un tipo listo &mdash;.respondi&oacute; &eacute;l.  &mdash; &iquest;Te gustar&iacute;a escapar de estas paredes? Te necesitamos &mdash; a&ntilde;adi&oacute;.<br>&mdash; &iquest;Qu&eacute; pasa, tienes algo preparado, viejo? &mdash; respond&iacute; con sorna, pensando que me estaba tomando el pelo."
 	],
 	indice = 0
 
@@ -18,15 +19,14 @@
 	$('.nav-left').on('click', evt => {
 		evt.preventDefault()
 
-		indice--
+		$('.nav-right').removeClass('disabled')
 
-		if (indice < pages.length) {
-			$('.nav-right').removeClass('disabled')
-		}
-
-		if (indice == 0) {
+		if (--indice == 0) {
 			$('.nav-left').addClass('disabled')
+			indice = 0
 		}
+
+		addPage()
 
 		return false
 	})
@@ -34,15 +34,14 @@
 	$('.nav-right').on('click', evt => {
 		evt.preventDefault()
 
-		indice++
+		$('.nav-left').removeClass('disabled')
 
-		if (indice > 0) {
-			$('.nav-left').removeClass('disabled')
-		}
-
-		if (indice == pages.length) {
+		if (++indice == pages.length) {
 			$('.nav-right').addClass('disabled')
+			indice = pages.length - 1
 		}
+
+		addPage()
 
 		return false
 	})
