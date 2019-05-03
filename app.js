e7c7fbb68cbcabf6720944a82aff73c428dffa10
Victor Rodriguez <guirto@gmail.com>
2019-05-03 20:00:09 +0100

    more

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 (function($) {
 	var pages = [
-	"Era un día normal en la cárcel. Todo lo normal que puede ser un día en un sitio así. Estábamos animados porque hoy algo cambiaba nuestra rutina. Unos amables señores venían a visitar nuestras celdas con los chicos de mantenimiento para valorar unas reformas."
+		"Era un día normal en la cárcel. Todo lo normal que puede ser un día en un sitio así. Estábamos animados porque hoy algo cambiaba nuestra rutina. Unos amables señores venían a visitar nuestras celdas con los chicos de mantenimiento para valorar unas reformas."
 	],
 	indice = 0
 
 	var addPage = (_indice) => {
 		let index = _indice || indice
 
-		$('#main').append("<p>" + pages[index] + "</p>")
+		$('#main')
+			.empty()
+			.append("<p>" + pages[index] + "</p>")
 	}
+
+	// métodos de manipulación del DOM
 
 	addPage()
 
