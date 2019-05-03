866ac83bd90342042d7589136545304a71a83dad
Victor Rodriguez <guirto@gmail.com>
2019-05-03 20:24:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 	var pages = [
 		"Era un d&iacute;a normal en la c&aacute;rcel. Todo lo normal que puede ser un d&iacute;a en un sitio as&iacute;. Est&aacute;bamos animados porque hoy algo cambiaba nuestra rutina. Unos amables se&ntilde;ores ven&iacute;an a visitar nuestras celdas con los chicos de mantenimiento para valorar unas reformas.",
 		"Nos hab&iacute;an hecho salir a la galer&iacute;a y no hab&iacute;a mucho que hacer all&iacute;, salvo rajar con los dem&aacute;s. Uno de los veteranos se acerc&oacute; y empezamos a hablar. Era un tipo raro. Nunca me hab&iacute;a dirigido la palabra.",
-		"Me extra&ntilde;&oacute; que me dijera:<br>&mdash; Llevo tiempo observ&aacute;ndote. &iquest;Te gusta estar aqu&iacute;?<br>&mdash; &iquest;T&uacute; qu&eacute; crees? Esto no tiene pinta de hotel' &mdash; respond&iacute; &mdash;. Adem&aacute;s, no deber&iacute;a estar aqu&iacute;: mi abogado la cag&oacute; &mdash; a&ntilde;ad&iacute; con un gui&ntilde;o.",
-		"&mdash; Lo que me imaginaba, un tipo listo &mdash;.respondi&oacute; &eacute;l.  &mdash; &iquest;Te gustar&iacute;a escapar de estas paredes? Te necesitamos &mdash; a&ntilde;adi&oacute;.<br>&mdash; &iquest;Qu&eacute; pasa, tienes algo preparado, viejo? &mdash; respond&iacute; con sorna, pensando que me estaba tomando el pelo."
+		"Me extra&ntilde;&oacute; que me dijera:<br><em>&mdash; Llevo tiempo observ&aacute;ndote. &iquest;Te gusta estar aqu&iacute;?<br>&mdash; &iquest;T&uacute; qu&eacute; crees? Esto no tiene pinta de hotel' &mdash;</em> respond&iacute; <em>&mdash;. Adem&aacute;s, no deber&iacute;a estar aqu&iacute;: mi abogado la cag&oacute; &mdash;</em> a&ntilde;ad&iacute; con un gui&ntilde;o.",
+		"<em>&mdash; Lo que me imaginaba, un tipo listo &mdash;</em>.respondi&oacute; &eacute;l.  <em>&mdash; &iquest;Te gustar&iacute;a escapar de estas paredes? Te necesitamos &mdash;</em> a&ntilde;adi&oacute;.<br><em>&mdash; &iquest;Qu&eacute; pasa, tienes algo preparado, viejo? &mdash;</em> respond&iacute; con sorna, pensando que me estaba tomando el pelo."
 	],
 	indice = 0
 
